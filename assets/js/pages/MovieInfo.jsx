56fc11edd38611ad2558c47517056a24a0e9f3c7
Tom <tom_dempster@hotmail.co.uk>
2019-06-16 18:50:53 +0100

    tidy up

--- assets/js/pages/MovieInfo.jsx
+++ assets/js/pages/MovieInfo.jsx
@@ -51,15 +51,6 @@ export default class MovieInfo extends React.Component {
             </div>
         );
     }
-    render(){
-        return(
-            <div style={{height: 'calc(100vh - 76px)'}} className="d-flex justify-content-center align-items-center">
-                <div className="spinner-border" role="status" style={{height: '10rem', width: '10rem', color: '#9628a7'}}>
-                    <span className="sr-only">Loading...</span>
-                </div>
-            </div>
-        );
-    }
 
     render() {
         if(this.state.loading) {
